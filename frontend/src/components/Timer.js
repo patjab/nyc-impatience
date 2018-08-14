90ab3cb1a3698a7b4a05da4cb7628b6d81936e41
github username <github email address>
2018-08-14 09:23:19 -0400

    formatted timer and score bar

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -14,14 +14,35 @@ class Timer extends Component {
     return {minutes: Math.trunc((this.state.time/100)/60), seconds: Math.trunc((this.state.time/100) % (60)), milliseconds: this.state.time % 100}
   }
 
+  formatMovement() {
+    return `${("0000" + this.props.movement).slice(-5)}`
+  }
+
   drawTimer = () => {
+    const statusBarHeight = 100
+
     const ctx = this.props.canvas ? this.props.canvas.getContext("2d") : null
     if (ctx) {
-      ctx.clearRect(canvasWidth-250, 0, 300, 100)
-      ctx.font = "30px Geneva"
-      ctx.fillStyle = "red"
+      ctx.clearRect(0, 0, canvasWidth, statusBarHeight)
+      ctx.fillStyle = 'black'
+      ctx.fillRect(0, 0, canvasWidth, statusBarHeight)
       const currentTime = this.formatTime()
-      ctx.fillText(`Time: ${("0" + currentTime.minutes).slice(-2)}:${("0" + currentTime.seconds).slice(-2)}.${("0" + currentTime.milliseconds).slice(-2)}`, canvasWidth-250, 30)
+
+      ctx.font = "20px Geneva"
+      ctx.fillStyle = "white"
+      ctx.fillText(`Steps Without Crashing`, 50, 30)
+
+      ctx.font = "36px Geneva"
+      ctx.fillStyle = "red"
+      ctx.fillText(`${this.formatMovement()}`, 100, 70)
+
+      ctx.font = "20px Geneva"
+      ctx.fillStyle = "white"
+      ctx.fillText(`Timer`, canvasWidth-200, 30)
+
+      ctx.font = "36px Geneva"
+      ctx.fillStyle = "red"
+      ctx.fillText(`${("0" + currentTime.minutes).slice(-2)}:${("0" + currentTime.seconds).slice(-2)}.${("0" + currentTime.milliseconds).slice(-2)}`, canvasWidth-250, 70)
     }
   }
 
@@ -37,7 +58,8 @@ class Timer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    canvas: state.canvas
+    canvas: state.canvas,
+    movement: state.movement
   }
 }
 
